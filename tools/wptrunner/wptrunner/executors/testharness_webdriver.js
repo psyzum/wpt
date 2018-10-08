50899b26d5112b5adf58150c785b083fceb64020
James Graham <james@hoppipolla.co.uk>
2018-10-08 13:41:15 +0100

    Don't switch back to the harness window during testharness tests.
    Previously we kept switching back and forth between the harness window
    and the window containing the test as we processed WebDriver
    callbacks. But this is unnecessary; we can use the fact that we have
    access to window.opener to set the callback on the opener window and
    run it there, yes still have it block a WebDriver call in the test
    window. This means that during the test we can keep the focus on the
    test window, but put all the complex logic in the parent and retain
    the ability to close the test window after each test to create a clean
    environment.

--- tools/wptrunner/wptrunner/executors/testharness_webdriver.js
+++ tools/wptrunner/wptrunner/executors/testharness_webdriver.js
@@ -1,6 +1,17 @@
+var callback = arguments[arguments.length - 1];
+var loaded = false;
+
 window.timeout_multiplier = %(timeout_multiplier)d;
 window.url = "%(url)s";
 window.win = window.open("%(abs_url)s", "%(window_id)s");
+window.win.addEventListener('pageshow', (e) => {
+  if (loaded || e.target.location.href == "about:blank") {
+    return;
+  }
+  complete = true;
+  callback();
+});
+
 
 window.message_queue = [];
 window.testdriver_callback = null;
@@ -10,4 +21,9 @@ if (%(timeout)s != null) {
     window.win.timeout();
     window.win.close();
   }, %(timeout)s);
+}
+
+if (window.win.location.href != 'about:blank') {
+  loaded = true;
+  callback();
 }
